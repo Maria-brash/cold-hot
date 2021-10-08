cafcec2e42e073a2988bc1286c5a0c592df59469
Maria-brash <maria.08.08.16@mail.ru>
2021-10-08 12:52:07 +0300

    Game

--- src/View.php
+++ src/View.php
@@ -1,7 +1,8 @@
-<?php 
-	namespace Maria_brash\cold_hot\View;
-	
-    function showGame() {
-        echo "Game shown".PHP_EOL;
-    }
-?>
+<?php
+
+namespace Maria_brash\cold_hot\View;
+
+function showGame()
+{
+    echo "Start game" . PHP_EOL;
+}
